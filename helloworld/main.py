41b7e196e19a336c64cf8dbb85f279ee27a68750
CintiaCardoso <cintianahicardoso@gmail.com>
2023-11-10 14:06:29 -0300

    added lastname in main

---
+++ helloworld/main.py
@@ -0,0 +1,8 @@
+def hello_world (request):
+    request_args = request.args 
+    if request_args and 'name' in request_args and 'lastname' in request_args:
+        name = request_args['name']
+        lastname = request_args['lastname']
+    else:
+        name ='World'
+    return f'Hello {name}{lastname}'
